e4462a17b73cea1350bf397b1fc0c41751f677cd
op2adi <adityaupadhyay2708@gmail.com>
2024-02-09 14:21:44 +0530

    Thida sa bacha hai

---
+++ mysqlfiles/dataentry.sql
@@ -0,0 +1,54 @@
+use makemytrip
+DELETE FROM booked_loungue;
+DELETE FROM complaint;
+DELETE FROM flight;
+DELETE FROM holiday_pay;
+DELETE FROM holidaypackage;
+DELETE FROM hotel_invouice;
+DELETE FROM hotels;
+DELETE FROM loungue;
+DELETE FROM payments;
+DELETE FROM tickets;
+DELETE FROM trains;
+DELETE FROM transport;
+DELETE FROM users;
+INSERT INTO Users (userid, email, name, phnumber, gender, Address_hno, City, Pincode, dob) VALUES (3337, stevensmith@example.com, Benjamin Jordan, 2228680636, F, 17712, North Dan, 803718, 1957-11-15 );
+INSERT INTO Users (userid, email, name, phnumber, gender, Address_hno, City, Pincode, dob) VALUES (1009, wharrison@example.org, Henry Rodriguez, 9953138526, M, 945, North Reginatown, 501140, 1964-08-30 );
+INSERT INTO Users (userid, email, name, phnumber, gender, Address_hno, City, Pincode, dob) VALUES (3184, morajorge@example.net, Robert Baker, 7378246364, F, 56688, Lake Nancy, 458136, 1991-07-27 );
+INSERT INTO Users (userid, email, name, phnumber, gender, Address_hno, City, Pincode, dob) VALUES (4831, jcampbell@example.org, Andrew Edwards, 3508994027, F, 055, West Tiffanyshire, 498394, 1970-01-11 );
+INSERT INTO Users (userid, email, name, phnumber, gender, Address_hno, City, Pincode, dob) VALUES (4978, thomas22@example.com, Gary Johnson, 4203936353, F, 4732, East Tammyhaven, 536651, 1973-08-14 );
+INSERT INTO Users (userid, email, name, phnumber, gender, Address_hno, City, Pincode, dob) VALUES (7174, tinamurphy@example.com, Judy Henry, 2358637911, F, 562, West Christopher, 327544, 1943-09-23 );
+INSERT INTO Users (userid, email, name, phnumber, gender, Address_hno, City, Pincode, dob) VALUES (8498, kramersteven@example.com, Jeffrey Shelton, 2201452450, M, 4237, Port Oscar, 355784, 1966-06-16 );
+INSERT INTO Users (userid, email, name, phnumber, gender, Address_hno, City, Pincode, dob) VALUES (2782, cody61@example.org, Alyssa Barnes, 5571084202, F, 8115, East Vincentbury, 426088, 1955-10-12 );
+INSERT INTO Users (userid, email, name, phnumber, gender, Address_hno, City, Pincode, dob) VALUES (3638, dustin51@example.com, Elizabeth Burke, 7623034956, F, 76828, Port Joseph, 445662, 1962-08-15 );
+INSERT INTO Users (userid, email, name, phnumber, gender, Address_hno, City, Pincode, dob) VALUES (2117, dennissmith@example.org, Erica Cruz, 7157660319, F, 5268, Foxmouth, 638506, 1983-02-20 );
+INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES (3492, 2782, Everyone change kitchen since hear glass begin person. Each then air adult develop why. Involve born reduce stand let check make year. Whose position size medical edge ago recently.);
+INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES (7913, 3337, Particularly travel also by but end. Use point must certainly more despite hour. Or beat science enough upon time.);
+INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES (6581, 3638, Sound system power themselves. Church relate environmental. Your director blood growth include group hear reflect.);
+INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES (1428, 4978, Out various again general food back. Offer film democratic begin whether certain size. Teacher yard man. Need glass accept move. White store base end street. Rich attorney fire sort. Once interest answer than.);
+INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES (3905, 3184, Whatever him quickly treat. Church certainly college thank bill such time. Food these land view. Next job management.);
+INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES (1724, 8498, Old very reach science exist their. Drug church our realize defense would increase. Billion above politics rise collection sit. Identify guess moment as picture despite model time.);
+INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES (5045, 3184, Mouth give visit good care drop you very. Range general person situation far. Shake particular point former card. Charge law very deep fill big. West eight money find fast commercial.);
+INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES (2368, 3638, Appear off radio listen sign letter. Serious produce might owner audience central idea. Kid national understand interview side question economic. Care half around.);
+INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES (3495, 4978, World walk trial reason kitchen fund sea. Up measure because memory itself financial window. Cold white part life. Father energy the pressure miss. Whether allow certainly approach operation mind customer. Reason wind civil something analysis.);
+INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES (5285, 7174, Seat evidence can field. Argue seat oil entire serious throughout concern standard. Artist away sort such little into laugh. Result take budget cell marriage morning leg. Seven allow sometimes. Theory crime free shoulder.);
+INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES (Zez8DnhM, ICICI Bank, 1981-12-01 14:56:54, 6689 Joshua Plains Apt. 933
+East Katherineport, IL 75585);
+INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES (MJEc0pfd, ICICI Bank, 2018-06-05 00:00:22, 11478 Elizabeth Stream Apt. 217   
+West Ashleyfort, HI 13669);
+INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES (VJXspZM9, HDFC Bank, 1976-12-06 14:11:16, 59982 Daniel Lodge Apt. 142
+West Aliciaside, KY 56551);
+INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES (IaFPAgvN, ICICI Bank, 2021-10-24 08:23:48, 3257 King Stravenue
+Wangbury, AK 93318);
+INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES (H00bLzL4, ICICI Bank, 1993-09-19 22:46:14, PSC 7795, Box 6926
+APO AP 82136);
+INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES (IjYShPuh, Kotak Mahindra Bank, 2012-08-05 01:58:06, 73036 Phillips Crossroad 
+Caseybury, VA 72479);
+INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES (oPwcOB6P, State Bank of India, 1981-05-30 03:00:59, 267 Hoover Roads Apt. 933
+Nguyenside, MH 84771);
+INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES (Gnc5cbup, Kotak Mahindra Bank, 1989-11-13 19:57:17, 844 Judy Expressway Apt. 193
+Lawsonton, MP 58041);
+INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES (127beC4F, Axis Bank, 1976-11-30 21:50:37, 79162 Perkins Fall
+Lake Marcville, PA 80630);
+INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES (9SiPdnqi, HDFC Bank, 2003-07-24 11:04:04, 229 Wong Plains
+Port Martinshire, NH 13221);
--- python_data_creation/datapopulate.py
+++ python_data_creation/datapopulate.py
@@ -7,27 +7,32 @@ import string
 import mysql.connector
 from faker import Faker
 
-
+transid = []
+# global declaratoiom for this 
 # print(os.environ.get("DB_PASSWORD"))
 # adi_con = ''
 def connect(print_result=False):
     # global adi_con
-    adi_con = mysql.connector.connect(host = "localhost", user = "root",passwd = "Aditya@1998") # here i connected it to my database dont forget to enter your password and uname phir kehte hai ki placemnent nhi lgti
+    adi_con = mysql.connector.connect(host="localhost", user="root",
+                                      passwd="Aditya@1998")  # here i connected it to my database dont forget to enter your password and uname phir kehte hai ki placemnent nhi lgti
     if print_result:
         print(adi_con.is_connected())
     return adi_con
 
-adi_con = connect() # you can set it to true 
 
-#main cursor from which query will be executed and value added
-cursor = adi_con.cursor() # cursor to execute queries global declaration
+adi_con = connect()  # you can set it to true
 
-def execute(query): # for execution of queries
+# main cursor from which query will be executed and value added
+cursor = adi_con.cursor()  # cursor to execute queries global declaration
+
+
+def execute(query):  # for execution of queries
     cursor.execute(query)
     return
 
 
-execute("use makemytrip") #temp execution 
+execute("use makemytrip")  # temp execution
+
 
 # first clear all tables
 def clear_all_tables():
@@ -35,14 +40,16 @@ def clear_all_tables():
         # Get a list of all tables in the database
         cursor.execute("SHOW TABLES")
         tables = cursor.fetchall()
-
+        print(tables)
         # Loop through each table and delete all records
         for table in tables:
             table_name = table[0]
-            if table_name!="gender_ref":
+            print(table_name)
+            if table_name != "gender_ref":
                 cursor.execute(f"DELETE FROM {table_name}")
+                print(f"DELETE FROM {table_name};")
                 adi_con.commit()
-                print(f"All records cleared from table {table_name} successfully!")
+                # print(f"All records cleared from table {table_name} successfully!")
 
     except mysql.connector.Error as err:
         print("Error:", err)
@@ -55,25 +62,27 @@ def generate_unique_userid(existing_userids):
         if userid not in existing_userids:
             return userid
 
+
 def generate_fake_data_for_users(num_records):
     fake = Faker()
     data = []
     for _ in range(num_records):
         email = fake.email()
         name = fake.name()
-        phnumber = fake.random_number(digits = 10)
-        while len(str(phnumber))!=10:
-            phnumber = fake.random_number(digits = 10)
+        phnumber = fake.random_number(digits=10)
+        while len(str(phnumber)) != 10:
+            phnumber = fake.random_number(digits=10)
         # print(phnumber)
         gender = random.choice(['M', 'F'])
         Address_hno = fake.building_number()
         City = fake.city()
-        Pincode = fake.random_number(digits =6)
-        while len(str(Pincode))!=6:
-            Pincode = fake.random_number(digits =6)
+        Pincode = fake.random_number(digits=6)
+        while len(str(Pincode)) != 6:
+            Pincode = fake.random_number(digits=6)
         dob = fake.date_of_birth(minimum_age=18, maximum_age=80).strftime('%Y-%m-%d')
         data.append((email, name, phnumber, gender, Address_hno, City, Pincode, dob))
     return data
+
 
 def enter_temp_data_for_users(data):
     try:
@@ -91,9 +100,11 @@ def enter_temp_data_for_users(data):
             val = (userid,) + record
             # print(val)
             cursor.execute(sql, val)
+            # print(sql,val+";")
+            print(f"INSERT INTO Users (userid, email, name, phnumber, gender, Address_hno, City, Pincode, dob) VALUES"+f" ({userid}, {record[0]}, {record[1]}, {record[2]}, {record[3]}, {record[4]}, {record[5]}, {record[6]}, {record[7]} )"+";")
             adi_con.commit()
 
-            print("Record inserted successfully!")
+            # print("Record inserted successfully!")
             existing_userids.add(userid)
 
     except mysql.connector.Error as err:
@@ -116,6 +127,7 @@ def generate_unique_complaint_id(existing_ids):
         if complaint_id not in existing_ids:
             return complaint_id
 
+
 def fetch_user_ids():
     user_ids = []
     try:
@@ -124,6 +136,7 @@ def fetch_user_ids():
     except mysql.connector.Error as err:
         print("Error:", err)
     return user_ids
+
 
 def generate_fake_complaint_data(num_complaints, user_ids):
     fake = Faker()
@@ -135,14 +148,19 @@ def generate_fake_complaint_data(num_complaints, user_ids):
         data.append((complaint_id, user_id, description))
     return data
 
+
 def enter_complaint_data(data):
     try:
         for record in data:
-            cursor.execute("INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES (%s, %s, %s)", record)
+            cursor.execute("INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES (%s, %s, %s)",
+            record)
+            print(f"INSERT INTO Complaint (complaint_id, user_id, complaint_description) VALUES ({record[0]}, {record[1]}, {record[2]})"+";")
             adi_con.commit()
-            print("Record inserted successfully!")
+            # print("Record inserted successfully!")
     except mysql.connector.Error as err:
         print("Error:", err)
+
+
 def add_complaints():
     # Fetch user IDs from the Users table
     user_ids = fetch_user_ids()
@@ -157,26 +175,40 @@ def add_complaints():
 def generate_offer_code():
     offer_code = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
     return offer_code
+def fetch_offer_code():
+    offer_codes = []
+    try:
+        cursor.execute("SELECT Offers_code FROM loungue")
+        offer_codes = [row[0] for row in cursor.fetchall()]
+    except mysql.connector.Error as err:
+        print("Error:", err)
+    return offer_codes
+
 
 def generate_fake_data_for_loungue(num_records):
     fake = Faker()
     data = []
     for _ in range(num_records):
         offer_code = generate_offer_code()
-        credit_card = fake.random_element(elements=('HDFC Bank', 'ICICI Bank', 'State Bank of India', 'Axis Bank', 'Kotak Mahindra Bank'))
+        credit_card = fake.random_element(
+            elements=('HDFC Bank', 'ICICI Bank', 'State Bank of India', 'Axis Bank', 'Kotak Mahindra Bank'))
         timings = fake.date_time()
         place = fake.address()
         data.append((offer_code, credit_card, timings, place))
     return data
 
+
 def enter_data_for_loungue(data):
     try:
         for record in data:
-            cursor.execute("INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES (%s, %s, %s, %s)", record)
+            cursor.execute("INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES (%s, %s, %s, %s)",
+                           record)
             adi_con.commit()
-            print("Record inserted successfully!")
+            print(f"INSERT INTO Loungue (Offers_code, Credit_Card, Timings, Place) VALUES ({record[0]}, {record[1]}, {record[2]}, {record[3]});")
+            # print("Record inserted successfully!")
     except mysql.connector.Error as err:
         print("Error:", err)
+
 
 def Lounge_data():
     num_records = 10
@@ -184,9 +216,81 @@ def Lounge_data():
 
     # Call the function to enter data
     enter_data_for_loungue(data)
+    
 
+
+def generate_transport_data():
+    fake = Faker()
+    k = 0
+    while True:
+        k = random.randint(1000000,10000000)
+        if k not in transid:
+            transid.append(k)
+            break
+
+    # flight_no = random.randint(1000,9999)
+    start_loc = fake.city()
+    destn_loc = fake.city()
+    while start_loc == destn_loc:
+        destn_loc = fake.city()
+    timings = fake.date_time_between(start_date='-1y', end_date='+1y')
+    price = random.randint(10, 1000)
+    return (k,start_loc, destn_loc, timings, price,100)  # Set destination to original start location
+
+def transport_data():
+    for i in range(10):
+        transport_data = generate_transport_data()
+        transport_data = list(transport_data)
+        insert_query = "INSERT INTO Transport (Transport_id, Start_Loc, Destn_Loc, Timings, Price, Vacany) VALUES (%s,%s, %s, %s, %s, %s)"
+        cursor.execute(insert_query, transport_data)
+        adi_con.commit()
+        print(f"INSERT INTO Transport (Transport_id, Start_Loc, Destn_Loc, Timings, Price, Vacany) VALUES {transport_data[0], transport_data[1], transport_data[2], transport_data[3], transport_data[4],transport_data[5]}"+";")
+        while transport_data[0] in transid:
+            transport_data[0]+=1
+        transid.append(transport_data[0])
+        transport_data[1],transport_data[2]=transport_data[2],transport_data[1]
+        cursor.execute(insert_query, transport_data)
+        adi_con.commit()
+        print(f"INSERT INTO Transport (Transport_id, Start_Loc, Destn_Loc, Timings, Price, Vacany) VALUES {transport_data[0], transport_data[2], transport_data[2], transport_data[3], transport_data[4],transport_data[5]}"+";")
+
+
+def fetch_transport_data():
+    cursor.execute("SELECT Transport_id FROM Transport")  # Replace 'attribute_column' with the actual attribute column name
+    transport_ids = [row[0] for row in cursor.fetchall()]
+    return transport_ids
+def generate_train_data(transport_ids):
+    trains = []
+    for i in range(5):
+        train_no = random.randint(1000, 9999)
+        transport_id = random.choice(transport_ids)
+        trains.append((train_no, transport_id))
+        transport_ids.remove(transport_id)  # Remove the used transport ID to ensure it's not used for flights
+    return trains
+
+# Function to generate flight data
+def generate_flight_data(transport_ids):
+    flights = []
+    for i in range(5):
+        flight_no = random.randint(1000, 9999)
+        transport_id = random.choice(transport_ids)
+        flights.append((flight_no, transport_id))
+        transport_ids.remove(transport_id)  # Remove the used transport ID to ensure it's not used for trains
+    return flights
+def train_Add():
+    transid = fetch_transport_data()
+    adi_badi = generate_train_data(transid)
+    for i in adi_badi:
+        print(i)
+
+
+
+
+def bookerd_Loungue():
+    userids = fetch_user_ids()
 if __name__ == '__main__':
     clear_all_tables()
-    adding_for_users()
-    add_complaints()
-    Lounge_data()
+    # adding_for_users()
+    # add_complaints()
+    # Lounge_data()
+    transport_data()
+    # train_Add()
